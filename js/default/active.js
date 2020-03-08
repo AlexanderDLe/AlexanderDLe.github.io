3f7409645234616b4773d453e4fcb5b5ea405148
alexanderdle <alexanderledev@gmail.com>
2020-03-07 23:56:28 -0800

    Remove ScrollUp Icon

--- js/default/active.js
+++ js/default/active.js
@@ -1,4 +1,4 @@
-(function ($) {
+(function($) {
     'use strict';
 
     var dornee_window = $(window);
@@ -7,42 +7,45 @@
     // :: 1.0 Preloader Active Code
     // ****************************
 
-    dornee_window.on('load', function () {
-        $('#preloader').fadeOut('1000', function () {
+    dornee_window.on('load', function() {
+        $('#preloader').fadeOut('1000', function() {
             $(this).remove();
         });
     });
 
     // When the user scrolls the page, execute myFunction
-    window.onscroll = function () {
-        scrollIndicatorFunction()
+    window.onscroll = function() {
+        scrollIndicatorFunction();
     };
 
     function scrollIndicatorFunction() {
-        var winScroll = document.body.scrollTop || document.documentElement.scrollTop;
-        var height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+        var winScroll =
+            document.body.scrollTop || document.documentElement.scrollTop;
+        var height =
+            document.documentElement.scrollHeight -
+            document.documentElement.clientHeight;
         var scrolled = (winScroll / height) * 100;
-        document.getElementById("scrollIndicator").style.width = scrolled + "%";
+        document.getElementById('scrollIndicator').style.width = scrolled + '%';
     }
 
     // ****************************
     // :: 2.0 Contact Form Code
     // ****************************
 
-    $("#contactMebtn").on('click', function () {
-        $(".popup-contact-form-area").toggleClass('active');
+    $('#contactMebtn').on('click', function() {
+        $('.popup-contact-form-area').toggleClass('active');
     });
 
-    $(".popup-close").on('click', function () {
-        $(".popup-contact-form-area").removeClass('active');
+    $('.popup-close').on('click', function() {
+        $('.popup-contact-form-area').removeClass('active');
     });
 
-    $(".menu-hideshow").on('click', function () {
-        $(".dornee-menu").toggleClass('active');
+    $('.menu-hideshow').on('click', function() {
+        $('.dornee-menu').toggleClass('active');
     });
 
-    $("#ham").on('click', function () {
-        $(".hamburger").toggleClass('active');
+    $('#ham').on('click', function() {
+        $('.hamburger').toggleClass('active');
     });
 
     // *********************************
@@ -58,32 +61,41 @@
             smartSpeed: 800,
             autoplayTimeout: 10000,
             nav: true,
-            navText: [('<i class="arrow_left"></i>'), ('<i class="arrow_right"></i>')]
-        })
+            navText: [
+                '<i class="arrow_left"></i>',
+                '<i class="arrow_right"></i>'
+            ]
+        });
 
-        welcomeSlider.on('translate.owl.carousel', function () {
-            var layer = $("[data-animation]");
-            layer.each(function () {
+        welcomeSlider.on('translate.owl.carousel', function() {
+            var layer = $('[data-animation]');
+            layer.each(function() {
                 var anim_name = $(this).data('animation');
-                $(this).removeClass('animated ' + anim_name).css('opacity', '0');
+                $(this)
+                    .removeClass('animated ' + anim_name)
+                    .css('opacity', '0');
             });
         });
 
-        $("[data-delay]").each(function () {
+        $('[data-delay]').each(function() {
             var anim_del = $(this).data('delay');
             $(this).css('animation-delay', anim_del);
         });
 
-        $("[data-duration]").each(function () {
+        $('[data-duration]').each(function() {
             var anim_dur = $(this).data('duration');
             $(this).css('animation-duration', anim_dur);
         });
 
-        welcomeSlider.on('translated.owl.carousel', function () {
-            var layer = welcomeSlider.find('.owl-item.active').find("[data-animation]");
-            layer.each(function () {
+        welcomeSlider.on('translated.owl.carousel', function() {
+            var layer = welcomeSlider
+                .find('.owl-item.active')
+                .find('[data-animation]');
+            layer.each(function() {
                 var anim_name = $(this).data('animation');
-                $(this).addClass('animated ' + anim_name).css('opacity', '1');
+                $(this)
+                    .addClass('animated ' + anim_name)
+                    .css('opacity', '1');
             });
         });
 
@@ -92,8 +104,12 @@
             var currentSlide = $('.welcome-slides .owl-item.active');
             currentSlide.next('.owl-item').addClass('next');
             currentSlide.prev('.owl-item').addClass('prev');
-            var nextSlideImg = $('.owl-item.next').find('.single-slide').attr('data-img-url');
-            var prevSlideImg = $('.owl-item.prev').find('.single-slide').attr('data-img-url');
+            var nextSlideImg = $('.owl-item.next')
+                .find('.single-slide')
+                .attr('data-img-url');
+            var prevSlideImg = $('.owl-item.prev')
+                .find('.single-slide')
+                .attr('data-img-url');
             $('.owl-nav .owl-prev').css({
                 background: 'url(' + prevSlideImg + ')'
             });
@@ -103,7 +119,7 @@
         }
 
         welcomeSlide();
-        welcomeSlider.on('translated.owl.carousel', function () {
+        welcomeSlider.on('translated.owl.carousel', function() {
             welcomeSlide();
         });
     }
@@ -122,8 +138,11 @@
             autoplayTimeout: 5000,
             dots: true,
             nav: true,
-            navText: [('<i class="arrow_left"></i>'), ('<i class="arrow_right"></i>')]
-        })
+            navText: [
+                '<i class="arrow_left"></i>',
+                '<i class="arrow_right"></i>'
+            ]
+        });
     }
 
     // *********************************
@@ -143,8 +162,11 @@
             mainClass: 'mfp-fade',
             preloader: true,
             callbacks: {
-                beforeOpen: function () {
-                    this.st.image.markup = this.st.image.markup.replace('mfp-figure', 'mfp-figure mfp-with-anim');
+                beforeOpen: function() {
+                    this.st.image.markup = this.st.image.markup.replace(
+                        'mfp-figure',
+                        'mfp-figure mfp-with-anim'
+                    );
                     this.st.mainClass = this.st.el.attr('data-effect');
                 }
             },
@@ -183,12 +205,12 @@
     // :: 10.0 Scrollup Active Code
     // ****************************
 
-    if ($.fn.scrollUp) {
-        dornee_window.scrollUp({
-            scrollSpeed: 1500,
-            scrollText: '<i class="arrow_carrot-up"</i>'
-        });
-    }
+    // if ($.fn.scrollUp) {
+    //     dornee_window.scrollUp({
+    //         scrollSpeed: 500,
+    //         scrollText: '<i class="arrow_carrot-up"</i>'
+    //     });
+    // }
 
     // ******************************
     // :: 11.0 Counter Up Active Code
@@ -205,7 +227,7 @@
     // :: 12.0 Prevent Default 'a' Click
     // *********************************
 
-    $('a[href="#"]').on('click', function ($) {
+    $('a[href="#"]').on('click', function($) {
         $.preventDefault();
     });
 
@@ -214,9 +236,9 @@
     // ************************************
 
     if ($.fn.imagesLoaded) {
-        $('.gallery_area').imagesLoaded(function () {
+        $('.gallery_area').imagesLoaded(function() {
             // Filter items on button click
-            $('.portfolio-menu').on('click', 'button', function () {
+            $('.portfolio-menu').on('click', 'button', function() {
                 var filterValue = $(this).attr('data-filter');
                 $grid.isotope({
                     filter: filterValue
@@ -234,10 +256,10 @@
     // :: 15.0 Gallery Menu Style Code
     // *******************************
 
-    $('.portfolio-menu button.btn').on('click', function () {
+    $('.portfolio-menu button.btn').on('click', function() {
         $('.portfolio-menu button.btn').removeClass('active');
         $(this).addClass('active');
-    })
+    });
 
     // ******************************
     // :: 16.0 onePageNav active code
@@ -245,7 +267,7 @@
 
     if ($.fn.onePageNav) {
         $('#nav').onePageNav({
-            currentClass: "active"
+            currentClass: 'active'
         });
     }
 
@@ -253,10 +275,13 @@
     // :: 17.0 ScrollDown Active Code
     // ******************************
 
-    $("#scrollDown").on('click', function () {
-        $('html, body').animate({
-            scrollTop: $("#about").offset().top - 0
-        }, 1000);
+    $('#scrollDown').on('click', function() {
+        $('html, body').animate(
+            {
+                scrollTop: $('#about').offset().top - 0
+            },
+            1000
+        );
     });
 
     // ******************************
@@ -295,11 +320,22 @@
     // :: 20.0 ScrollDown Active Code
     // ******************************
 
-    $(".modal").each(function (l) {
-        $(this).on("show.bs.modal", function (l) {
-            var o = $(this).attr("data-easein");
-            "shake" == o ? $(".modal-dialog").velocity("callout." + o) : "pulse" == o ? $(".modal-dialog").velocity("callout." + o) : "tada" == o ? $(".modal-dialog").velocity("callout." + o) : "flash" == o ? $(".modal-dialog").velocity("callout." + o) : "bounce" == o ? $(".modal-dialog").velocity("callout." + o) : "swing" == o ? $(".modal-dialog").velocity("callout." + o) : $(".modal-dialog").velocity("transition." + o)
-        })
+    $('.modal').each(function(l) {
+        $(this).on('show.bs.modal', function(l) {
+            var o = $(this).attr('data-easein');
+            'shake' == o
+                ? $('.modal-dialog').velocity('callout.' + o)
+                : 'pulse' == o
+                ? $('.modal-dialog').velocity('callout.' + o)
+                : 'tada' == o
+                ? $('.modal-dialog').velocity('callout.' + o)
+                : 'flash' == o
+                ? $('.modal-dialog').velocity('callout.' + o)
+                : 'bounce' == o
+                ? $('.modal-dialog').velocity('callout.' + o)
+                : 'swing' == o
+                ? $('.modal-dialog').velocity('callout.' + o)
+                : $('.modal-dialog').velocity('transition.' + o);
+        });
     });
-
 })(jQuery);
